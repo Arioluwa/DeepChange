fbe3d9670f2ae8428f9fa4329aef0f84e4731672
Adebowale <dansy37@gmail.com>
2021-12-14 17:55:42 +0100

    merged gapfilled_dates scripts

--- scripts/cmd.sh
+++ scripts/cmd.sh
@@ -4,3 +4,5 @@
 #python data_processing/processingchain3.py --atile ../../data/theia2A_zip_img/2019 --output_dir ../../data/theiaL2A_zip_img/output --preview True >> logs/log202112071700.txt
 
 python data_processing/processingchain.py --atile ../../data/theiaL2A_zip_img/2019 --output_dir ../../data/theiaL2A_zip_img/output/ --gfd data_processing/gapfilled19_dates.txt --shf ../../data/samples_shapefiles/samples_oso2019_T31TCJ.shp --preview True >> logs/log202112072125.txt
+
+python data_processing/processingchain.py --atile ../../data/theiaL2A_zip_img/2018 --output_dir ../../data/theiaL2A_zip_img/output/ --gfd data_processing/gapfilled18_dates.txt --shf ../../data/samples_shapefiles/samples_oso2018_T31TCJ.shp --preview True >> logs/log202112141715.txt
