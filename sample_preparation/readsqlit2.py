20d27fc2a49c1fb90b78aebd84fae06e14467ec9
Adebowale <dansy37@gmail.com>
2022-01-20 00:44:07 +0100

    sqlite extract script updated

--- sample_preparation/readsqlit2.py
+++ sample_preparation/readsqlit2.py
@@ -47,10 +47,13 @@ def readSITSData(chunk):
     polygonID_data = chunk.iloc[:,4]
     polygon_ids = np.asarray(polygonID_data.values, dtype='uint16')
 
+    block_id_data = chunk.iloc[:,5]
+    block_ids = np.asarray(block_id_data.values, dtype='uint8')
+
     X_data = chunk.iloc[:,6:]
     X = np.asarray(X_data.values, dtype='uint16')
 
-    return  X, polygon_ids, y
+    return  X, polygon_ids, block_ids ,y
 
 
 # Sqlite connection
@@ -60,13 +63,15 @@ conn = sqlite3.connect(f_path)
 X = [] # Explanatory variables
 y = [] # Labels/codes
 polygon_ids = [] # Polygon ids
+block_ids = []
 
 start_time = time.time()
 # read the data in chunks
 for chunk in pd.read_sql_query("select * from output;", conn,  chunksize=chunk_size):
-    X_data, polygon_ids_data, y_data = readSITSData(chunk)
+    X_data, polygon_ids_data, block_ids_data, y_data = readSITSData(chunk)
     X.append(X_data)
     y.append(y_data)
+    block_ids.append(block_ids_data)
     polygon_ids.append(polygon_ids_data)
 print("read and append %s seconds ---" % (time.time() - start_time))
 # convert lists to numpy arrays
@@ -74,6 +79,7 @@ start_time = time.time()
 X = np.concatenate(X)
 y = np.concatenate(y)
 polygon_ids = np.concatenate(polygon_ids)
+block_ids = np.concatenate(block_ids)
 print("concatenate %s seconds ---" % (time.time() - start_time))
 
 # get file sqlite name base to save the npz file; for example: "2018_sample_selection.sqlite" -> 2018
@@ -83,18 +89,18 @@ f_name = f_name.split('_')[0]
 
 start_time = time.time()
 # save X, y, polygon_ids in a single .npy file
-np.savez_compressed(os.path.join(output_dir, '%s_SITS_data.npz' % f_name), X=X, y=y, polygon_ids=polygon_ids)
+np.savez_compressed(os.path.join(output_dir, '%s_SITS_data.npz' % f_name), X=X, y=y, block_id = block_ids, polygon_ids=polygon_ids)
 print("npz compression %s seconds ---" % (time.time() - start_time))
 
-def load_npz(file_path):
-    """
-    Load data from a .npz file
-    """
-    with np.load(file_path) as data:
-        X = data['X']
-        y = data['y']
-        polygon_ids = data['polygon_ids']
-    return X, y, polygon_ids
+# def load_npz(file_path):
+#     """
+#     Load data from a .npz file
+#     """
+#     with np.load(file_path) as data:
+#         X = data['X']
+#         y = data['y']
+#         polygon_ids = data['polygon_ids']
+#     return X, y, polygon_ids
 
 #start_time = time.time()
 #X, y, polygon_ids = load_npz(os.path.join(output_dir, '%s_SITS_data.npz' % f_name))
