a97493c9215d7435b4908b9c38e29b7001b88d6f
adebowaledaniel <dansy37@gmail.com>
2022-03-24 17:48:30 +0100

    dataloader updates

--- ltae/utils.py
+++ ltae/utils.py
@@ -17,42 +17,13 @@ def load_npz(file_path):
 
 ######### Read Train, Validation, and Evaluation ids #########
 
-def read_ids(file_path):
-    """
-    Read ids from file
-    """
-    with open(file_path, "r") as f:
-        lines = f.readlines()
-        Train_ids = eval(lines[0].split(":")[1])
-        test_ids = eval(lines[1].split(":")[1])
-        # Eval_ids = eval(lines[2].split(":")[1])
-    return Train_ids, test_ids#, Eval_ids
-
-######### Load different sets of data #########
-
-# train_ids, val_ids = read_ids(_train_val_eval)
-
-# X, Y, polygon_ids, block_ids = load_npz("2018_SITS_data.npz")
-
-# total_set = np.concatenate((X, Y[:, None], block_ids[:, None]), axis=1)
-# def load_set(set = "train", total_set):
+# def read_ids(file_path):
 #     """
-#     Load training, validation and evaluation set
+#     Read ids from file
 #     """
-#     if set == "train":
-#         Trainingset = total_set[np.isin(total_set[:, -1], train_ids)]
-#         Xtrain = Trainingset[:, :-2]
-#         Ytrain = Trainingset[:, -2]
-#         return Xtrain, Ytrain
-#     elif set == "val":
-#         Validset = total_set[np.isin(total_set[:, -1], val_ids)]
-#         Xval = Validset[:, :-2]
-#         Yval = Validset[:, -2]
-#         return Xval, Yval
-#     # elif set == "eval":
-#     #     Evalset = total_set[np.isin(total_set[:, -1], eval_ids)]
-#     #     Xeval = Evalset[:, :-2]
-#     #     Yeval = Evalset[:, -2]
-#     #     return Xeval, Yeval
-#     else:
-#         raise ValueError("set must be either 'train' or 'val'")
+#     with open(file_path, "r") as f:
+#         lines = f.readlines()
+#         Train_ids = eval(lines[0].split(":")[1])
+#         test_ids = eval(lines[1].split(":")[1])
+#         Eval_ids = eval(lines[2].split(":")[1])
+#     return Train_ids, test_ids, Eval_ids
