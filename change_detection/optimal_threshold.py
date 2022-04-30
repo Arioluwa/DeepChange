777512fc0d356f42847482b1dab43e1f564a806f
adebowaledaniel <dansy37@gmail.com>
2022-04-30 18:15:21 +0200

    updated optimal threshold script

--- change_detection/optimal_threshold.py
+++ change_detection/optimal_threshold.py
@@ -1,5 +1,7 @@
 import os
 import time
+import argparse
+import pprint
 import rasterio
 import numpy as np
 import pandas as pd
@@ -15,13 +17,7 @@ from sklearn.metrics import f1_score
 from sklearn.metrics import auc
 
 
-def optm_threshold(
-    similarity_map,
-    case,
-    gt_source,
-    gt_target, 
-    threshold: bool,
-    outdir):
+def main(args):
     
     """
     Desc: Change detection using similarity measure (Euclidean distance)
@@ -37,16 +33,20 @@ def optm_threshold(
         Confusion matrix,
         and dataframe of fscore for each threshold
     """
+    pprint.pprint(vars(args))
+    prepare_output(args)
+    
+    outdir = args.outdir
     
     # read all images to array
     # similarity_array = rasterio.open(similarity_map).read(1)
-    with rasterio.open(similarity_map) as src:
+    with rasterio.open(args.similarity) as src:
         similarity_array = src.read(1)
         profile = src.profile
         profile['nodata'] = 0.0
     
-    gt_source_ = rasterio.open(gt_source).read(1)
-    gt_target_ = rasterio.open(gt_target).read(1)
+    gt_source_ = rasterio.open(args.gt_source).read(1)
+    gt_target_ = rasterio.open(args.gt_target).read(1)
     
     # get gt mask (where there are value) and binary (change/no-chnage)
     gt_mask = (gt_source_ != 0) & (gt_target_ != 0)
@@ -66,7 +66,7 @@ def optm_threshold(
     # model_name = '_'.join(model_name)
       # model_name
     
-    if threshold == True:
+    if not args.otsu:
         thresholds = np.linspace(similarity_.min(), similarity_.max(), 10)
 
         # initiate metrics
@@ -104,7 +104,7 @@ def optm_threshold(
         plt.ylabel('Precision')
         plt.legend()
         plt.title( 'AUC={0:0.2f}'.format(auc(recall_, precision_)))#plt.title(model_name +'_Precision-Recall curve, AUC={0:0.2f},'.format(auc(recall_, precision_)))
-        plt.savefig(os.path.join(outdir, 'case_' + case + '_precision-recall.png'))
+        plt.savefig(os.path.join(outdir, './charts', 'case_' + args.case + '_precision-recall.png'), dpi=500)
         plt.close()
 
         # plot ROC curve 
@@ -112,24 +112,24 @@ def optm_threshold(
         plt.plot(fpr, tpr)
         plt.xlabel('False positive rate')
         plt.ylabel('True positi/ve rate')
-        plt.title('case_' + case + '_ROC curve')
-        plt.savefig(os.path.join(outdir, 'case_' + case + '_roc_curve.png'))
+        plt.title('case_' + args.case + '_ROC curve')
+        plt.savefig(os.path.join(outdir,'./charts', 'case_' + args.case + '_roc_curve.png'), dpi=500)
         plt.close()
 
     #     # get the optimal threshold based on fscore
         df = pd.DataFrame({'threshold':thresholds, 'fscore':fscore_})
-        df.to_csv(os.path.join(outdir, 'case_' + case + '_fscore.csv'), index=False)
+        df.to_csv(os.path.join(outdir, 'case_' + args.case + '_fscore.csv'), index=False)
 
         #confusion
         cm_sim = confusion_matrix(gt_binary_, np.where(similarity_ >= opt_threshold, 1, 0))
         cm_sim_per = cm_sim.astype('float') / np.sum(cm_sim)
 
         label = ['No change', 'Change']
-        cm_sim_plot = sns.heatmap(cm_sim_per, annot=True, fmt ='.2%', cmap='Greens', xticklabels=label, yticklabels=label, cbar=False)
+        cm_sim_plot = sns.heatmap(cm_sim_per, annot=True, fmt ='.2%', cmap='Blues', xticklabels=label, yticklabels=label, cbar=False, annot_kws={"size": 30})
         cm_sim_plot.set(xlabel= "Predicted", ylabel= "Ground truth")
         # cm_sim_plot.set_title("")
         # save the plot
-        plt.savefig(os.path.join('./charts', 'similarity_confusion_matrix_case_' + case +'.png'))
+        plt.savefig(os.path.join(outdir,'./charts', 'similarity_confusion_matrix_case_' + args.case +'.png'), dpi=500)
         plt.close()
 
         # similarity histogram distribution
@@ -139,89 +139,134 @@ def optm_threshold(
         # plt.yticks([])
         plt.legend()
         # save chart
-        plt.savefig(os.path.join('./charts', 'similarity_distribution_case_' + case +'.png'))
+        plt.savefig(os.path.join(outdir,'./charts', 'similarity_distribution_case_' + args.case +'.png'), dpi = 500)
         plt.close()
-
-        # similarity binary change
-        similarity_binary = np.where(similarity_ >= opt_threshold, 2, 1)
-        similarity_binary_change = np.empty_like(gt_binary) # shape: 10980, 10980
-        similarity_binary_change[~gt_binary_mask.mask] = similarity_binary.ravel()
-
-        ## write to raster
-        with rasterio.open(os.path.join(outdir, 'similarity_bin_change_case_' + case + '.tif'), 'w', **profile) as dst:
-            dst.write(similarity_binary_change, 1)
-            dst.close()
-
-
-        ## change map
-        # Note:
-        # 0 = nodata
-        # 1 = (0 == 1) ~ No change = No change
-        # 2 = (0 == 2) ~ No change = Change
-        # 3 = (1 == 1) ~ Change = No change
-        # 4 = (1 == 2) ~ Change = Change
-
-        ## change array
-        change_array = np.empty_like(gt_binary_) # flatten shape
-        change_array[(gt_binary_ == 0) & (similarity_binary == 1)] = 1
-        change_array[(gt_binary_ == 0) & (similarity_binary == 2)] = 2
-        change_array[(gt_binary_ == 1) & (similarity_binary == 1)] = 3
-        change_array[(gt_binary_ == 1) & (similarity_binary == 2)] = 4
-
-        change_map = np.empty_like(gt_binary) # shape: 10980, 10980
-        change_map[~gt_binary_mask.mask] = change_array.ravel()
-
-        print(np.unique(change_map))
-        with rasterio.open(os.path.join(outdir, 'sim-change_map_case_' + case + '.tif'), 'w', **profile) as dst:
-            dst.write(change_map, 1)
-            dst.close()
-    else:
-        threshold = threshold_otsu(similarity_)
-        otsu_binary = similarity_ > threshold
         
-        # # plot precision-recall curve
-        # plt.figure()
-        # plt.plot(recall_, precision_, label='Precision-Recall curve')
-        # plt.plot(recall_[opt_threshold_idx], precision_[opt_threshold_idx], 'o', markersize=5, color='r', label='Optimal threshold: {0:0.4f}'.format(opt_threshold))
-        # plt.xlabel('Recall')
-        # plt.ylabel('Precision')
-        # plt.legend()
-        # plt.title( 'AUC={0:0.2f}'.format(auc(recall_, precision_)))#plt.title(model_name +'_Precision-Recall curve, AUC={0:0.2f},'.format(auc(recall_, precision_)))
-        # plt.savefig(os.path.join(outdir, model_name + '_precision-recall.png'))
-        # plt.close()
+        if args.map:
+            # similarity binary change
+            similarity_binary = np.where(similarity_ >= opt_threshold, 2, 1)
+            similarity_binary_change = np.empty_like(gt_binary) # shape: 10980, 10980
+            similarity_binary_change[~gt_binary_mask.mask] = similarity_binary.ravel()
 
-        # # plot ROC curve 
-        # plt.figure()
-        # plt.plot(fpr, tpr)
-        # plt.xlabel('False positive rate')
-        # plt.ylabel('True positi/ve rate')
-        # plt.title(model_name + '_ROC curve')
-        # plt.savefig(os.path.join(outdir, model_name + '_roc_curve.png'))
-        # plt.close()
+            ## write to raster
+            with rasterio.open(os.path.join(outdir, 'similarity_bin_change_case_' + args.case + '.tif'), 'w', **profile) as dst:
+                dst.write(similarity_binary_change, 1)
+                dst.close()
+
+
+            ## change map
+            # Note:
+            # 0 = nodata
+            # 1 = (0 == 1) ~ No change = No change
+            # 2 = (0 == 2) ~ No change = Change
+            # 3 = (1 == 1) ~ Change = No change
+            # 4 = (1 == 2) ~ Change = Change
+
+            ## change array
+            change_array = np.empty_like(gt_binary_) # flatten shape
+            change_array[(gt_binary_ == 0) & (similarity_binary == 1)] = 1
+            change_array[(gt_binary_ == 0) & (similarity_binary == 2)] = 2
+            change_array[(gt_binary_ == 1) & (similarity_binary == 1)] = 3
+            change_array[(gt_binary_ == 1) & (similarity_binary == 2)] = 4
+
+            change_map = np.empty_like(gt_binary) # shape: 10980, 10980
+            change_map[~gt_binary_mask.mask] = change_array.ravel()
+
+            print(np.unique(change_map))
+            with rasterio.open(os.path.join(outdir, 'sim-change_map_case_' + args.case + '.tif'), 'w', **profile) as dst:
+                dst.write(change_map, 1)
+                dst.close()
+    else:
+        opt_threshold = threshold_otsu(similarity_)
+        otsu_binary = similarity_ > opt_threshold
+        
+        # save otsu f1score
         fscore = f1_score(gt_binary_, otsu_binary)
-        f = open(os.path.join('otsu_case_' + case + '_fscore.txt'), 'w')
+        f = open(os.path.join('otsu_case_' + args.case + '_fscore.txt'), 'w')
         f.write('F1 score: {}'.format(fscore))
 
-        #confusion
+        #confusion matrix
         cm_sim = confusion_matrix(gt_binary_, otsu_binary)
         cm_sim_per = cm_sim.astype('float') / np.sum(cm_sim)
 
         label = ['No change', 'Change']
-        cm_sim_plot = sns.heatmap(cm_sim_per, annot=True, fmt ='.2%', cmap='Greens', xticklabels=label, yticklabels=label, cbar=False)
+        cm_sim_plot = sns.heatmap(cm_sim_per, annot=True, fmt ='.2%', cmap='Blues', xticklabels=label, yticklabels=label, cbar=False, annot_kws={"size": 30})
         cm_sim_plot.set(xlabel= "Predicted", ylabel= "Ground truth")
         # cm_sim_plot.set_title("")
         # save the plot
-        plt.savefig(os.path.join('./charts', 'otsu_similarity_confusion_matrix_case_' + case +'.png'))
+        plt.savefig(os.path.join(outdir, './charts', 'otsu_similarity_confusion_matrix_case_' + args.case +'.png'), dpi =500)
         plt.close()
         
-        
+        if args.map:
+            # similarity binary change
+            similarity_binary = np.where(similarity_ >= opt_threshold, 2, 1)
+            similarity_binary_change = np.empty_like(gt_binary) # shape: 10980, 10980
+            similarity_binary_change[~gt_binary_mask.mask] = similarity_binary.ravel()
+
+            ## write to raster
+            with rasterio.open(os.path.join(outdir, 'otsu_similarity_bin_change_case_' + args.case + '.tif'), 'w', **profile) as dst:
+                dst.write(similarity_binary_change, 1)
+                dst.close()
+
+
+                ## change map
+                # Note:
+                # 0 = nodata
+                # 1 = (0 == 1) ~ No change = No change
+                # 2 = (0 == 2) ~ No change = Change
+                # 3 = (1 == 1) ~ Change = No change
+                # 4 = (1 == 2) ~ Change = Change
+
+            ## change array
+            change_array = np.empty_like(gt_binary_) # flatten shape
+            change_array[(gt_binary_ == 0) & (similarity_binary == 1)] = 1
+            change_array[(gt_binary_ == 0) & (similarity_binary == 2)] = 2
+            change_array[(gt_binary_ == 1) & (similarity_binary == 1)] = 3
+            change_array[(gt_binary_ == 1) & (similarity_binary == 2)] = 4
+
+            change_map = np.empty_like(gt_binary) # shape: 10980, 10980
+            change_map[~gt_binary_mask.mask] = change_array.ravel()
+
+            print(np.unique(change_map))
+            with rasterio.open(os.path.join(outdir, 'otsu_sim-change_map_case_' + case + '.tif'), 'w', **profile) as dst:
+                dst.write(change_map, 1)
+                dst.close()
+
+def prepare_output(args):
+        os.makedirs(args.outdir, exist_ok=True)
+        os.makedirs(os.path.join(args.outdir, './charts'), exist_ok=True)
 
 if __name__ == '__main__':
-    gt_source = '../../../data/rasterized_samples/2018_rasterizedImage.tif'
-    gt_target = '../../../data/rasterized_samples/2019_rasterizedImage.tif'
+    
+#     gt_source = '../../../data/rasterized_samples/2018_rasterizedImage.tif'
+#     gt_target = '../../../data/rasterized_samples/2019_rasterizedImage.tif'
 
-    for case in ['4']:#['1', '2', '3', '4']:
-        similarity_map = '../../../results/RF/simliarity_measure/case_'+ case +'_ref_mask_similarity_measure.tif'
-        outdir = '../../../results/RF/simliarity_measure/optimal_threshold'
-        optm_threshold(similarity_map, case, gt_source, gt_target, False, outdir)
+#     for case in ['4']:#['1', '2', '3', '4']:
+#         similarity_map = '../../../results/RF/simliarity_measure/case_'+ case +'_ref_mask_similarity_measure.tif'
+#         outdir = '../../../results/RF/simliarity_measure/optimal_threshold'
+#         optm_threshold(similarity_map, case, gt_source, gt_target, False, outdir)
         
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('--outdir', '-o', default='../../../results/RF/simliarity_measure/optimal_threshold', type=str, help='Path to save files.')
+    parser.add_argument('--case', '-c', default=2, type=str, help='Cases 1 to 4')
+    parser.add_argument('--gt_source', '-gs', default='../../../data/rasterized_samples/2018_rasterizedImage.tif', type=str, help='Source ground truth path')
+    parser.add_argument('--gt_target', '-gp', default='../../../data/rasterized_samples/2019_rasterizedImage.tif', type=str, help='Target ground truth path')
+    parser.add_argument('--similarity', '-s', default='../../../results/RF/simliarity_measure/case_2_ref_mask_similarity_measure.tif', type=str, help='similarity map')
+    parser.add_argument('--otsu', '-ot', default=False, type=bool, help='Compute optimal threshold using otsu-threshold')
+    parser.add_argument('--map', '-m', default=False, type=bool, help='generate maps')
+    
+    
+    
+    args = parser.parse_args()
+    # execute 
+    # for case in range(1, 4):
+    #     args.case = str(case)
+    #     args.similarity = '../../../results/RF/simliarity_measure/case_{}_ref_mask_similarity_measure.tif'.format(case)
+    #     main(args)
+    
+    # case 4
+    # args.case = "4"
+    # args.similarity = '../../../results/RF/simliarity_measure/case_4_ref_mask_similarity_measure.tif'
+    # main(args)
+    
