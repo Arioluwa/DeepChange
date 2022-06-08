7b2c0f6315bb7e26230b608a4584e0624eb2198f
adebowaledaniel <dansy37@gmail.com>
2022-06-08 20:10:59 +0200

    update binary change detection script

--- change_detection/Binary_change_detection.py
+++ change_detection/Binary_change_detection.py
@@ -82,19 +82,19 @@ def main(args):
     # plot the change matrix with percent or not 
     # print(cm[0,1])
     if args.percent:
-        cm = cm.astype('float') / np.sum(cm)
-        cm_plot = sns.heatmap(cm, annot=True, fmt='.2%', cmap='Blues', xticklabels=label, yticklabels=label, cbar=False, annot_kws={"size": 30})
-        cm_plot.set(xlabel= "Predicted", ylabel= "Ground truth")
+        cm_per = cm.astype('float') / np.sum(cm)
+        cm_plot_per = sns.heatmap(cm_per, annot=True, fmt='.2%', cmap='Blues', xticklabels=label, yticklabels=label, cbar=False, annot_kws={"size": 30})
+        cm_plot_per.set(xlabel= "Predicted", ylabel= "Ground truth")
         # save the plot
         plt.savefig(os.path.join(outdir,'./charts','bcd_change_matrix_percent_case_' + args.case +'.png'), dpi = 500)
         plt.close()
-    else:
+    # else:
         cm_plot = sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=label, yticklabels=label, cbar=False, annot_kws={"size": 30})
         cm_plot.set(xlabel= "Predicted", ylabel= "Ground truth")
 
         for t in cm_plot.texts:
             t.set_text('{:,d}'.format(int(t.get_text())))
-        # print(cm_plot)
+        
         # save the plot
         plt.savefig(os.path.join(outdir, './charts','bcd_change_matrix_case_' + args.case +'.png'), dpi = 500)
         plt.close()
@@ -108,6 +108,8 @@ def main(args):
                 f.write("\n Total error: {}".format((cm[0,1] + cm[1,0])))
                 f.write("\n OA: {}".format(((cm[0,0] + cm[1,1])/(cm[0,0] +cm[0,1]+cm[1,0]+cm[1,1]))))
                 f.write("\n fscore: {}".format(f_score))
+                f.write("\n Error matrix in percent \n")
+                f.write(str(cm_per))
                 f.close()
         quality_check()
 
@@ -120,31 +122,31 @@ def main(args):
     # 4 = (2 == 2) ~ Change = Change
 
     #change array - 
-#     change_array = np.empty_like(gt_binary_values) # as the same dimension as gt_binary_values
-#     change_array[(gt_binary_values == 1) & (pred_binary_values == 1)] = 1
-#     change_array[(gt_binary_values == 1) & (pred_binary_values == 2)] = 2
-#     change_array[(gt_binary_values == 2) & (pred_binary_values == 1)] = 3
-#     change_array[(gt_binary_values == 2) & (pred_binary_values == 2)] = 4
+    change_array = np.empty_like(gt_binary_values) # as the same dimension as gt_binary_values
+    change_array[(gt_binary_values == 1) & (pred_binary_values == 1)] = 1
+    change_array[(gt_binary_values == 1) & (pred_binary_values == 2)] = 2
+    change_array[(gt_binary_values == 2) & (pred_binary_values == 1)] = 3
+    change_array[(gt_binary_values == 2) & (pred_binary_values == 2)] = 4
 
 #     # change map
-#     change_map = np.empty_like(gt_binary)
-#     change_map[~gt_binary_mask.mask] = change_array.ravel() # returns 
+    change_map = np.empty_like(gt_binary)
+    change_map[~gt_binary_mask.mask] = change_array.ravel() # returns 
 
-    # write the change map
-    # with rasterio.open(os.path.join(outdir, 'change_map_case'+ args.case +'.tif'), 'w', **profile) as dst:
-    #     dst.write(change_map, 1)
+    ## write the change map
+    with rasterio.open(os.path.join(outdir, 'change_map_case'+ args.case +'.tif'), 'w', **profile) as dst:
+        dst.write(change_map, 1)
     print("--- %s minutes ---" % ((time.time() - starttime) / 60))
     
 #     ## this is just to compute the fscore for pred_binary; this is needed to be compared with 
 #     ## fscore from threshold of similarity measure
-#     gt_binary_values[gt_binary_values ==1] = 0
-#     gt_binary_values[gt_binary_values ==2] = 1
-#     pred_binary_values[pred_binary_values ==1] = 0
-#     pred_binary_values[pred_binary_values ==2] = 1
+    gt_binary_values[gt_binary_values ==1] = 0
+    gt_binary_values[gt_binary_values ==2] = 1
+    pred_binary_values[pred_binary_values ==1] = 0
+    pred_binary_values[pred_binary_values ==2] = 1
     
-    # fscore = f1_score(gt_binary_values, pred_binary_values)
-    # f = open(os.path.join('./charts/', 'delcase_' + args.case + '_fscore.txt'), 'w')
-    # f.write('F1 score: {}'.format(fscore))
+    fscore = f1_score(gt_binary_values, pred_binary_values)
+    f = open(os.path.join('./charts/', 'delcase_' + args.case + '_fscore.txt'), 'w')
+    f.write('F1 score: {}'.format(fscore))
 def prepare_output(args):
         os.makedirs(args.outdir, exist_ok=True)
         os.makedirs(os.path.join(args.outdir, './charts'), exist_ok=True)
@@ -156,42 +158,44 @@ if __name__ == '__main__':
     parser.add_argument('--case', '-c', default=2, type=str, help='Cases 1 to 4')
     parser.add_argument('--gt_source', '-gs', default='../../../data/rasterized_samples/2018_rasterizedImage.tif', type=str, help='Source ground truth path')
     parser.add_argument('--gt_target', '-gp', default='../../../data/rasterized_samples/2019_rasterizedImage.tif', type=str, help='Target ground truth path')
-    parser.add_argument('--pred_source', '-ps', default='../../../results/RF/2018_rf_case_2_map.tif', type=str, help='Source classification map')
-    parser.add_argument('--pred_target', '-pt', default='../../../results/RF/2019_rf_case_3_map.tif', type=str, help='Target  classification map')
-    parser.add_argument('--percent', '-p', default=False, type=bool, help='Cal percent in the confusion matrix')
+    parser.add_argument('--pred_source', '-ps', default='../../../results/ltae/classificationmap/2018_LTAE_case_1_map.tif', type=str, help='Source classification map')
+    parser.add_argument('--pred_target', '-pt', default='../../../results/ltae/classificationmap/2019_LTAE_case_2_map.tif', type=str, help='Target  classification map')
+    parser.add_argument('--percent', '-p', default=True, type=bool, help='Cal percent in the confusion matrix')
     
     args = parser.parse_args()
-    main(args)
+    # main(args)
     # args = vars(args) # if needed as a dict... call as args['case']
-    
+    # results/RF/classificationmap/2018_rf_case_1_map.tif
     # execute for RF
     # for case in range(1, 4):
     #     args.case = str(case)
-    #     args.pred_source = '../../../results/RF/2018_rf_case_{}_map.tif'.format(case)
-    #     args.pred_target = '../../../results/RF/2019_rf_case_{}_map.tif'.format(case)
+    #     args.outdir = '../../../results/RF/bcd'
+    #     args.pred_source = '../../../results/RF/classificationmap/2018_rf_case_{}_map.tif'.format(case)
+    #     args.pred_target = '../../../results/RF/classificationmap/2019_rf_case_{}_map.tif'.format(case)
     #     main(args)
     #     print('Case {} done'.format(case))
-    # # case 4
+    # case 4
     # args.case = "4"
+    # args.outdir = '../../../results/RF/bcd'
     # # args.percent = True
-    # args.pred_source = '../../../results/RF/2018_rf_case_2_map.tif'
-    # args.pred_target = '../../../results/RF/2019_rf_case_3_map.tif'
+    # args.pred_source = '../../../results/RF/classificationmap/2018_rf_case_1_map.tif'
+    # args.pred_target = '../../../results/RF/classificationmap/2019_rf_case_2_map.tif'
     # main(args)
     # print('Case {} done'.format(args.case))
-    
+    # results/ltae/classificationmap/2018_LTAE_case_{}_map.tif
     #ltae
     # for case in range(2,4):
     #     args.case = str(case)
-    #     args.pred_source = '../../../results/ltae/classificationmap/Seed_0/2018_LTAE_map_case_{}.tif'.format(case)
-    #     args.pred_target = '../../../results/ltae/classificationmap/Seed_0/2019_LTAE_map_case_{}.tif'.format(case)
+    #     args.pred_source = '../../../results/ltae/classificationmap/2018_LTAE_case_{}_map.tif'.format(case)
+    #     args.pred_target = '../../../results/ltae/classificationmap/2019_LTAE_case_{}_map.tif'.format(case)
     #     args.outdir = "../../../results/ltae/Change_detection/bcd"
     #     main(args)
     #     print('Case {} done'.format(case))
-    # # # case 4
-    # args.case = "4"
-    # # args.percent = True
-    # args.pred_source = '../../../results/ltae/classificationmap/Seed_0/2018_LTAE_map_case_2.tif'
-    # args.pred_target = '../../../results/ltae/classificationmap/Seed_0/2019_LTAE_map_case_3.tif'
-    # args.outdir = "../../../results/ltae/Change_detection/bcd"
-    # main(args)
-    # print('Case {} done'.format(args.case))
+    # # case 4
+    args.case = "1"
+    # args.percent = True
+    args.pred_source = '../../../results/ltae/classificationmap/2018_LTAE_case_1_map.tif'
+    args.pred_target = '../../../results/ltae/classificationmap/2019_LTAE_case_1_map.tif'
+    args.outdir = "../../../results/ltae/Change_detection/bcd"
+    main(args)
+    print('Case {} done'.format(args.case))
